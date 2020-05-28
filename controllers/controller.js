5416f2ab03da60e88d1a4e54cafe34e946577fd5
Blanca Frade <blancafradep@gmail.com>
2020-05-28 10:52:38 +0200

    developed fase1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,20 @@
 "use strict";
-function createCar(plate, brand, color) {
-    var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
+var car;
+// DOM Events
+var productForm = document.getElementById('product-form');
+if (productForm !== null) {
+    productForm.addEventListener('submit', function (e) {
+        var plate = document.getElementById('plate').value;
+        var brand = document.getElementById('brand').value;
+        var color = document.getElementById('color').value;
+        // Create a new Oject Car
+        var car = new Car(plate, brand, color);
+        // car.addWheel(new Wheel(2,"SEAT"));
+        var carInfo = document.getElementById('carInfo');
+        var element = document.createElement('div');
+        element.innerHTML = "\n            <div class=\"card text-center mb-4\">\n                <div class=\"card-body\">\n                    <strong>PLATE:</strong> " + car.plate + " -\n                    <strong>COLOR:</strong> " + car.color + " - \n                    <strong>BRAND:</strong> " + car.brand + " - \n                    <strong>WHEELS:</strong> " + JSON.stringify(car.wheels) + "\n                    <a href=\"#\" class=\"btn btn-danger\" name=\"delete\">Add Wheels</a>\n                </div>\n            </div>\n        ";
+        console.log(element);
+        carInfo.appendChild(element);
+        e.preventDefault();
+    });
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,38 @@
 
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+let car: Car;
+
+// DOM Events
+
+let productForm = document.getElementById('product-form');
+if (productForm !== null) {
+    productForm.addEventListener('submit', function (e) {
+        const plate: string = document.getElementById('plate').value;
+        const brand:string = document.getElementById('brand').value;
+        const color:string = document.getElementById('color').value;
+           
+        // Create a new Oject Car
+        car = new Car(plate, brand, color);
+    
+        // car.addWheel(new Wheel(2,"SEAT"));
+
+        const carInfo = document.getElementById('carInfo');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class="card-body">
+                    <strong>PLATE:</strong> ${car.plate} -
+                    <strong>COLOR:</strong> ${car.color} - 
+                    <strong>BRAND:</strong> ${car.brand} - 
+                    <strong>WHEELS:</strong> ${JSON.stringify(car.wheels)}
+                    <a href="#" class="btn btn-danger" name="delete">Add Wheels</a>
+                </div>
+            </div>
+        `;
+        carInfo.appendChild(element);
+
+    
+        e.preventDefault();
+    });
+
 }
 
