29eff5228f835f985b60a90936079dd15cfde7b5
Blanca Frade <blancafradep@gmail.com>
2020-06-03 11:15:22 +0200

    changed the way addwheels was developed

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,59 +1,56 @@
 "use strict";
 var car;
+var cars = new Array();
+var ui;
 // DOM Events
 var carForm = document.getElementById('car-form');
-if (carForm !== null) {
-    carForm.addEventListener('submit', function (e) {
-        if (carValidate()) {
-            var plate = document.getElementById('plate').value;
-            var brand = document.getElementById('brand').value;
-            var color = document.getElementById('color').value;
-            // Create a new Oject Car
-            car = new Car(plate, brand, color);
-            var carInfo = document.getElementById('carInfo');
-            var element = document.createElement('div');
-            element.innerHTML = "\n                <div class=\"card text-center mb-4\">\n                    <div class=\"card-body\">\n                        <strong>PLATE:</strong> " + car.plate + " -\n                        <strong>COLOR:</strong> " + car.color + " - \n                        <strong>BRAND:</strong> " + car.brand + " - \n                        <strong>WHEELS:</strong> " + JSON.stringify(car.wheels) + "\n                        <a href=\"#\" class=\"btn btn-danger\" name=\"add_wheels\">Add Wheels</a>\n                    </div>\n                </div>\n            ";
-            carInfo.appendChild(element);
-            addWheels();
-        }
-        e.preventDefault();
-    });
-}
-function addWheels() {
-    function showWheelForm() {
-        var wheelsInfo = document.querySelector('.wheels-info');
-        wheelsInfo === null || wheelsInfo === void 0 ? void 0 : wheelsInfo.classList.remove('d-none');
+carForm === null || carForm === void 0 ? void 0 : carForm.addEventListener('submit', function (e) {
+    if (carValidate()) {
+        var plate = document.getElementById('plate').value;
+        var brand = document.getElementById('brand').value;
+        var color = document.getElementById('color').value;
+        // Create a new Oject Car
+        car = new Car(plate, brand, color);
+        cars.push(car);
+        ui = new UI();
+        ui.addCar(car);
+        ui.resetForm();
     }
-    var addWheels = document.querySelectorAll('a[name=add_wheels]');
-    if (addWheels.length) {
-        addWheels.forEach(function (addWheel) {
-            addWheel.addEventListener('click', showWheelForm);
-            var wheelsForm = document.getElementById('wheels-form');
-            if (wheelsForm !== null) {
-                wheelsForm.addEventListener('submit', function (e) {
-                    if (wheelValidate()) {
-                        var brand1 = document.getElementById('brand1').value;
-                        var diameter1 = document.getElementById('diameter1').value;
-                        var brand2 = document.getElementById('brand2').value;
-                        var diameter2 = document.getElementById('diameter2').value;
-                        var brand3 = document.getElementById('brand3').value;
-                        var diameter3 = document.getElementById('diameter3').value;
-                        var brand4 = document.getElementById('brand4').value;
-                        var diameter4 = document.getElementById('diameter4').value;
-                        // Create a new Oject Wheel
-                        car.addWheel(new Wheel(diameter1, brand1));
-                        car.addWheel(new Wheel(diameter2, brand2));
-                        car.addWheel(new Wheel(diameter3, brand3));
-                        car.addWheel(new Wheel(diameter4, brand4));
-                        var carInfo = document.getElementById('carInfo');
-                        carInfo === null || carInfo === void 0 ? void 0 : carInfo.removeChild(carInfo.firstChild);
-                        var element = document.createElement('div');
-                        element.innerHTML = "\n                            <div class=\"card text-center mb-4\">\n                                <div class=\"card-body\">\n                                    <strong>PLATE:</strong> " + car.plate + " -\n                                    <strong>COLOR:</strong> " + car.color + " - \n                                    <strong>BRAND:</strong> " + car.brand + " - \n                                    <strong>WHEELS:</strong> " + JSON.stringify(car.wheels) + "\n                                </div>\n                            </div>\n                        ";
-                        carInfo.appendChild(element);
-                    }
-                    e.preventDefault();
-                });
+    e.preventDefault();
+});
+var carInfo = document.getElementById('carInfo');
+carInfo === null || carInfo === void 0 ? void 0 : carInfo.addEventListener('click', function (e) {
+    var thisTarget = e.target;
+    if (thisTarget.name === 'add_wheels') {
+        var wheelsInfo_1 = document.querySelector('.wheels-info');
+        wheelsInfo_1 === null || wheelsInfo_1 === void 0 ? void 0 : wheelsInfo_1.classList.remove('d-none');
+        var currElement = e.currentTarget;
+        var currPlate_1 = currElement.querySelector('.car-plate').textContent;
+        var wheelsForm_1 = document.getElementById('wheels-form');
+        wheelsForm_1.addEventListener('submit', function (event) {
+            if (wheelValidate()) {
+                var brand1 = document.getElementById('brand1').value;
+                var diameter1 = document.getElementById('diameter1').value;
+                var brand2 = document.getElementById('brand2').value;
+                var diameter2 = document.getElementById('diameter2').value;
+                var brand3 = document.getElementById('brand3').value;
+                var diameter3 = document.getElementById('diameter3').value;
+                var brand4 = document.getElementById('brand4').value;
+                var diameter4 = document.getElementById('diameter4').value;
+                var currCar = cars.find(function (car) { return car.plate === currPlate_1; });
+                // Create a new Oject Wheel
+                currCar.addWheel(new Wheel(diameter1, brand1));
+                currCar.addWheel(new Wheel(diameter2, brand2));
+                currCar.addWheel(new Wheel(diameter3, brand3));
+                currCar.addWheel(new Wheel(diameter4, brand4));
+                var carInfo_1 = document.getElementById('carInfo');
+                var element = document.createElement('div');
+                element.innerHTML = "\n                    <div class=\"card text-center mb-4\">\n                        <div class=\"card-body\">\n                            <strong>PLATE:</strong> " + currCar.plate + " -\n                            <strong>COLOR:</strong> " + currCar.color + " - \n                            <strong>BRAND:</strong> " + currCar.brand + " - \n                            <strong>WHEELS:</strong> " + JSON.stringify(currCar.wheels) + "\n                        </div>\n                    </div>\n                ";
+                carInfo_1.replaceChild(element, thisTarget.closest('.card').parentElement);
+                wheelsForm_1.reset();
+                wheelsInfo_1 === null || wheelsInfo_1 === void 0 ? void 0 : wheelsInfo_1.classList.add('d-none');
             }
+            event.preventDefault();
         });
     }
-}
+});
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,91 +1,77 @@
-
 let car: Car;
+let cars:Car[] = new Array();
+let ui: UI;
 
 // DOM Events
 let carForm = document.getElementById('car-form');
-if (carForm !== null) {
-    carForm.addEventListener('submit', function (e) {
-        if(carValidate()) {
-            const plate: string = document.getElementById('plate').value;
-            const brand:string = document.getElementById('brand').value;
-            const color:string = document.getElementById('color').value;
-               
-            // Create a new Oject Car
-            car = new Car(plate, brand, color);
-        
-    
-            const carInfo = document.getElementById('carInfo');
-            const element = document.createElement('div');
-            element.innerHTML = `
-                <div class="card text-center mb-4">
-                    <div class="card-body">
-                        <strong>PLATE:</strong> ${car.plate} -
-                        <strong>COLOR:</strong> ${car.color} - 
-                        <strong>BRAND:</strong> ${car.brand} - 
-                        <strong>WHEELS:</strong> ${JSON.stringify(car.wheels)}
-                        <a href="#" class="btn btn-danger" name="add_wheels">Add Wheels</a>
-                    </div>
-                </div>
-            `;
-            carInfo.appendChild(element);  
-            addWheels();  
-        }
-        e.preventDefault();
-    });
-}
+carForm?.addEventListener('submit', function (e) {
+    if(carValidate()) {
+        const plate: string = document.getElementById('plate').value;
+        const brand:string = document.getElementById('brand').value;
+        const color:string = document.getElementById('color').value;
+            
+        // Create a new Oject Car
+        car = new Car(plate, brand, color);
+        cars.push(car);
 
-function addWheels() {
-    function showWheelForm() {
+        ui = new UI();
+        ui.addCar(car);
+        ui.resetForm();
+    }
+    e.preventDefault();
+});
+
+
+
+const carInfo = document.getElementById('carInfo');
+carInfo?.addEventListener('click', function(e) {
+    let thisTarget = e.target;
+    if(thisTarget.name === 'add_wheels') {
         let wheelsInfo = document.querySelector('.wheels-info');
         wheelsInfo?.classList.remove('d-none');
-    
-    }
-    let addWheels = document.querySelectorAll('a[name=add_wheels]');
-    if(addWheels.length) {
-        addWheels.forEach(addWheel => {
-            addWheel.addEventListener('click', showWheelForm);
-            let wheelsForm = document.getElementById('wheels-form');
-            if (wheelsForm !== null) {
+        let currElement = e.currentTarget;
+        let currPlate = currElement.querySelector('.car-plate').textContent;
+
+        let wheelsForm = document.getElementById('wheels-form');
                 
-                wheelsForm.addEventListener('submit', function (e) {
-                    if(wheelValidate()) {
-                        const brand1: string = document.getElementById('brand1').value;
-                        const diameter1: number = document.getElementById('diameter1').value;
-                        const brand2: string = document.getElementById('brand2').value;
-                        const diameter2: number = document.getElementById('diameter2').value;
-                        const brand3: string = document.getElementById('brand3').value;
-                        const diameter3: number = document.getElementById('diameter3').value;
-                        const brand4: string = document.getElementById('brand4').value;
-                        const diameter4: number = document.getElementById('diameter4').value;
-                        
-                        // Create a new Oject Wheel
-                        car.addWheel(new Wheel(diameter1, brand1));
-                        car.addWheel(new Wheel(diameter2, brand2));
-                        car.addWheel(new Wheel(diameter3, brand3));                
-                        car.addWheel(new Wheel(diameter4, brand4));
+        wheelsForm.addEventListener('submit', function (event) {
+            if(wheelValidate()) {
+                const brand1: string = document.getElementById('brand1').value;
+                const diameter1: number = document.getElementById('diameter1').value;
+                const brand2: string = document.getElementById('brand2').value;
+                const diameter2: number = document.getElementById('diameter2').value;
+                const brand3: string = document.getElementById('brand3').value;
+                const diameter3: number = document.getElementById('diameter3').value;
+                const brand4: string = document.getElementById('brand4').value;
+                const diameter4: number = document.getElementById('diameter4').value;
 
-                        const carInfo = document.getElementById('carInfo');
-                        carInfo?.removeChild(carInfo.firstChild);
-                        const element = document.createElement('div');
-                        element.innerHTML = `
-                            <div class="card text-center mb-4">
-                                <div class="card-body">
-                                    <strong>PLATE:</strong> ${car.plate} -
-                                    <strong>COLOR:</strong> ${car.color} - 
-                                    <strong>BRAND:</strong> ${car.brand} - 
-                                    <strong>WHEELS:</strong> ${JSON.stringify(car.wheels)}
-                                </div>
-                            </div>
-                        `;
-                        carInfo.appendChild(element);  
-                    }
-                    e.preventDefault();
-                });
+                let currCar = cars.find(car => car.plate === currPlate);
+                
+                // Create a new Oject Wheel
+                currCar.addWheel(new Wheel(diameter1, brand1));
+                currCar.addWheel(new Wheel(diameter2, brand2));
+                currCar.addWheel(new Wheel(diameter3, brand3));                
+                currCar.addWheel(new Wheel(diameter4, brand4));
+                const carInfo = document.getElementById('carInfo');
+                const element = document.createElement('div');
+                element.innerHTML = `
+                    <div class="card text-center mb-4">
+                        <div class="card-body">
+                            <strong>PLATE:</strong> ${currCar.plate} -
+                            <strong>COLOR:</strong> ${currCar.color} - 
+                            <strong>BRAND:</strong> ${currCar.brand} - 
+                            <strong>WHEELS:</strong> ${JSON.stringify(currCar.wheels)}
+                        </div>
+                    </div>
+                `;
+                carInfo.replaceChild(element, thisTarget.closest('.card').parentElement;
+                wheelsForm.reset();
+                wheelsInfo?.classList.add('d-none');
             }
-
-
+            event.preventDefault();
 
         });
-    }    
-}
+    }
+})
+
 
